b761dfc4095f6afa4e585bea64328c2705a8ad22
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-02-25 00:08:38 +0800

    Update cmake_generate_projects.py

--- physx/buildtools/cmake_generate_projects.py
+++ physx/buildtools/cmake_generate_projects.py
@@ -266,7 +266,6 @@ class CMakePreset:
                 outString = outString + ' -DCMAKE_TOOLCHAIN_FILE=' + \
                     os.environ['ANDROID_NDK_HOME'] + \
                     '/build/cmake/android.toolchain.cmake'
-                outString = outString + ' -DANDROID_STL=\"system\"'
                 outString = outString + ' -DCM_ANDROID_FP=\"softfp\"'
                 outString = outString + ' -DANDROID_NDK=' + \
                     os.environ['ANDROID_NDK_HOME']
