e155e6ee73de3c29e6e60a1186f862e05bc2b5f1
bibi09456 <55490658+bibi09456@users.noreply.github.com>
2025-06-22 13:27:43 +0700

    Revert "Update cmake_generate_projects.py"
    This reverts commit e2600d42b8757f8ed1cced1cf491fa5f572f4d04.

--- physx/buildtools/cmake_generate_projects.py
+++ physx/buildtools/cmake_generate_projects.py
@@ -384,7 +384,7 @@ def presetProvided(pName, physx_root_dir):
                   physx_root_dir + '/compiler/' + cmakeMasterDir + '\"' + cmakeParams)
         os.chdir(physx_root_dir)
     else:
-        configs = ['release']
+        configs = ['debug', 'checked', 'profile', 'release']
         for config in configs:
             # cleanup and create output directory
             outputDir = os.path.join(physx_root_dir, 'compiler', parsedPreset.presetName + '-' + config)
@@ -395,7 +395,7 @@ def presetProvided(pName, physx_root_dir):
             os.chdir(outputDir)
             # print(cmakeExec + ' \"' + physx_root_dir + '/compiler/' + cmakeMasterDir + '\"' + cmakeParams + ' -DCMAKE_BUILD_TYPE=' + config)
             os.system(cmakeExec + ' \"' + physx_root_dir + '/compiler/' +
-                      cmakeMasterDir + '\"' + cmakeParams + ' -DCMAKE_BUILD_TYPE=RelWithDebInfo')
+                      cmakeMasterDir + '\"' + cmakeParams + ' -DCMAKE_BUILD_TYPE=' + config)
             os.chdir(physx_root_dir)
     pass
 
