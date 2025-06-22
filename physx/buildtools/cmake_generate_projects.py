379ec33f097293c9837f73b8eea8bb55eb771977
bibi09456 <55490658+bibi09456@users.noreply.github.com>
2025-06-23 00:04:15 +0700

    Update cmake_generate_projects.py

--- physx/buildtools/cmake_generate_projects.py
+++ physx/buildtools/cmake_generate_projects.py
@@ -312,6 +312,7 @@ class CMakePreset:
                 outString = outString + ' -DCMAKE_TOOLCHAIN_FILE=' + \
                     os.environ['ANDROID_NDK_HOME'] + \
                     '/build/cmake/android.toolchain.cmake'
+                outString = outString + ' -DCMAKE_SHARED_LINKER_FLAGS=\"-fuse-ld=lld -Wl,-O2 -Wl,--lto-O2 -Wl,--strip-debug\"'
                 outString = outString + ' -DCMAKE_VERBOSE_MAKEFILE=\"ON\"'
                 outString = outString + ' -DANDROID_STL=\"c++_static\"'
                 outString = outString + ' -DCM_ANDROID_FP=\"hard\"'
