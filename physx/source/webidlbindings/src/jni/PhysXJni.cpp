b5ed290226a03da2aff8d23fb4bd703c175f0464
bibi09456 <55490658+bibi09456@users.noreply.github.com>
2025-06-23 11:58:00 +0700

    Update PhysXJni.cpp

--- physx/source/webidlbindings/src/jni/PhysXJni.cpp
+++ physx/source/webidlbindings/src/jni/PhysXJni.cpp
@@ -8,4 +8,14 @@
 
 #include "PhysXJniGlue.h"
 
+#if PX_ANDROID
+extern "C" {
+extern int __llvm_profile_write_file(void);
+}
+
+void __attribute__((destructor)) write_profile_on_exit(void) {
+    __llvm_profile_write_file();
+}
+#endif
+
 // nothing else to do here, the magic happens inside PhysXJniGlue.h
