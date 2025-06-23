469f2f961eb6c38b099a8a0d7bf4fefec6f4edf1
bibi09456 <55490658+bibi09456@users.noreply.github.com>
2025-06-23 12:43:42 +0700

    Update PhysXJni.cpp

--- physx/source/webidlbindings/src/jni/PhysXJni.cpp
+++ physx/source/webidlbindings/src/jni/PhysXJni.cpp
@@ -9,12 +9,21 @@
 #include "PhysXJniGlue.h"
 
 #if PX_ANDROID
+#include <signal.h>
 extern "C" {
 extern int __llvm_profile_write_file(void);
 }
 
-void __attribute__((destructor)) write_profile_on_exit(void) {
+void profile_signal_handler(int sig) {
     __llvm_profile_write_file();
+    printf("PGO profile written\n");
+    exit(sig);
+}
+
+__attribute__((constructor)) void setup_profile_handler(void) {
+    signal(SIGTERM, profile_signal_handler);
+    signal(SIGINT, profile_signal_handler);
+    printf("Profile handler set up\n");
 }
 #endif
 
