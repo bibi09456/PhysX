c1a994b5b7cbbf1d65c958b9c2fddce30e6d0e40
bibi09456 <55490658+bibi09456@users.noreply.github.com>
2025-07-11 14:25:03 +0700

    add dummy init function

--- physx/buildtools/cmake_generate_projects.py
+++ physx/buildtools/cmake_generate_projects.py
@@ -312,7 +312,7 @@ class CMakePreset:
                 outString = outString + ' -DCMAKE_TOOLCHAIN_FILE=' + \
                     os.environ['ANDROID_NDK_HOME'] + \
                     '/build/cmake/android.toolchain.cmake'
-                outString = outString + ' -DCMAKE_SHARED_LINKER_FLAGS=\"-fuse-ld=lld -Wl,-q -Wl,--strip-debug -Wl,--init=_init\"'
+                outString = outString + ' -DCMAKE_SHARED_LINKER_FLAGS=\"-fuse-ld=lld -Wl,-q -Wl,--strip-debug -Wl,--undefined=_dummy_init_impl -Wl,--init=_dummy_init_impl\"'
                 outString = outString + ' -DCMAKE_VERBOSE_MAKEFILE=\"ON\"'
                 outString = outString + ' -DANDROID_STL=\"c++_static\"'
                 outString = outString + ' -DCM_ANDROID_FP=\"hard\"'
