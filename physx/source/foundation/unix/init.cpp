80444a70b211a86280b1cb33954030bfce9f95d8
bibi09456 <55490658+bibi09456@users.noreply.github.com>
2025-08-06 23:55:22 +0700

    Revert "add dummy init function"
    This reverts commit c1a994b5b7cbbf1d65c958b9c2fddce30e6d0e40.

--- physx/source/foundation/unix/init.cpp
+++ physx/source/foundation/unix/init.cpp
@@ -1,18 +1,1 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// Function with compiler attributes to prevent stripping
-__attribute__((used, noinline, section(".text.init")))
-void _dummy_init_impl() {
-    // Volatile assembly to prevent optimization removal
-    asm volatile("");
-}
-
-// Force reference in .init_array section
-__attribute__((used, section(".init_array")))
-void (*_dummy_init_ref)() = _dummy_init_impl;
-
-#ifdef __cplusplus
-}
-#endif
+void _init(void) { }
